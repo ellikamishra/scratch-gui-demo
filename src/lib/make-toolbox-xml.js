edad3ae091078a54c417c6956a8511be0880d5e2
ellikamishra <ellikamishra009@gmail.com>
2020-11-13 12:12:25 +0530

    scratch-update

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -685,6 +685,13 @@ const operators = function (isInitialSetup) {
                 </shadow>
             </value>
         </block>
+        <block type="operator_sqrt">
+            <value name="NUM">
+                <shadow type="math_number">
+                    <field name="NUM"/>
+                </shadow>
+            </value>
+        </block>
         ${blockSeparator}
         <block type="operator_mathop">
             <value name="NUM">
